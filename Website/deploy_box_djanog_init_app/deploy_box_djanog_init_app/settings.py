308d293e888480018ad6aefefa0a651927b0cef9
WernkeJD <Jacob.Wernke@yahoo.com>
2025-02-19 21:21:44 -0500

    shuffled secrete key and made it secrete

--- Website/deploy_box_djanog_init_app/deploy_box_djanog_init_app/settings.py
+++ Website/deploy_box_djanog_init_app/deploy_box_djanog_init_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w8ly6ot1om5y)_hu+4%jnd-pg&_^hx16#thvo*2&kl+2v!@23a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
