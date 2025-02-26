6da030f78edcf7334da0dc41fb257103da088005
WernkeJD <Jacob.Wernke@yahoo.com>
2025-02-25 21:31:18 -0500

    trying to fix db

--- Website/deploy_box_djanog_init_app/deploy_box_djanog_init_app/settings.py
+++ Website/deploy_box_djanog_init_app/deploy_box_djanog_init_app/settings.py
@@ -96,8 +96,15 @@ WSGI_APPLICATION = 'deploy_box_djanog_init_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        
     }
 }
 
