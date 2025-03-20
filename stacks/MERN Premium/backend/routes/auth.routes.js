0ca200675d4bdb7161aa1c31d72f972ec18983ba
Kaleb Bishop <kalebwbishop@gmail.com>
2025-03-20 15:18:33 -0400

    Updated paths

--- stacks/MERN Premium/backend/routes/auth.routes.js
+++ stacks/MERN Premium/backend/routes/auth.routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 //Imporing the authvalidation functions for login and register 
-const {  registerValidation, loginValidation} = require("../middleware")
+const {  registerValidation, loginValidation} = require("../middleware/middleware")
 //Importing functions from auth controller
 const { login, register, userProfile, users} = require("../controllers/auth.controller")
 //Importing the JWT verifyer from auth middleware 
-const verifyToken = require("../auth.middleware") 
+const verifyToken = require("../middleware/auth.middleware") 
 
 //Register route with register validation 
 router.post("/register", registerValidation, register);
--- stacks/MERN Premium/frontend/entrypoint.sh
+++ stacks/MERN Premium/frontend/entrypoint.sh
@@ -3,4 +3,7 @@
 # Enter REACT_APP_BACKEND_URL in config.js
 sed -i "s|REACT_APP_BACKEND_URL_PLACEHOLDER|${REACT_APP_BACKEND_URL}|g" /usr/share/nginx/html/config.js
 
-nginx -g 'daemon off;'
+serve -s build -l 8080
+
+# TODO: Run nginx again
+# nginx -g 'daemon off;'
